e86851d8ad9450dd19e97a16954b81a51d9dfc73
William <william13051992@gmail.com>
2017-06-22 16:26:59 +0200

    Uploading Visual Studio Files Test
    Testing uploading TickTacToe Game files

---
+++ TickTacToe/TickTacToe/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace TickTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.BTNA1 = new System.Windows.Forms.Button();
+            this.BTNA2 = new System.Windows.Forms.Button();
+            this.BTNA3 = new System.Windows.Forms.Button();
+            this.BTNB1 = new System.Windows.Forms.Button();
+            this.BTNB2 = new System.Windows.Forms.Button();
+            this.BTNB3 = new System.Windows.Forms.Button();
+            this.BTNC2 = new System.Windows.Forms.Button();
+            this.BTNC1 = new System.Windows.Forms.Button();
+            this.BTNC3 = new System.Windows.Forms.Button();
+            this.WM_background = new System.ComponentModel.BackgroundWorker();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(517, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // BTNA1
+            // 
+            this.BTNA1.BackColor = System.Drawing.Color.LightGray;
+            this.BTNA1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNA1.Location = new System.Drawing.Point(135, 143);
+            this.BTNA1.Name = "BTNA1";
+            this.BTNA1.Size = new System.Drawing.Size(75, 75);
+            this.BTNA1.TabIndex = 1;
+            this.BTNA1.TabStop = false;
+            this.BTNA1.UseVisualStyleBackColor = false;
+            this.BTNA1.Click += new System.EventHandler(this.BTNClicked);
+            this.BTNA1.MouseEnter += new System.EventHandler(this.BTN_Enter);
+            this.BTNA1.MouseLeave += new System.EventHandler(this.BTN_Leave);
+            // 
+            // BTNA2
+            // 
+            this.BTNA2.BackColor = System.Drawing.Color.LightGray;
+            this.BTNA2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNA2.Location = new System.Drawing.Point(216, 143);
+            this.BTNA2.Name = "BTNA2";
+            this.BTNA2.Size = new System.Drawing.Size(75, 75);
+            this.BTNA2.TabIndex = 2;
+            this.BTNA2.TabStop = false;
+            this.BTNA2.UseVisualStyleBackColor = false;
+            this.BTNA2.Click += new System.EventHandler(this.BTNClicked);
+            this.BTNA2.MouseEnter += new System.EventHandler(this.BTN_Enter);
+            this.BTNA2.MouseLeave += new System.EventHandler(this.BTN_Leave);
+            // 
+            // BTNA3
+            // 
+            this.BTNA3.BackColor = System.Drawing.Color.LightGray;
+            this.BTNA3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNA3.Location = new System.Drawing.Point(297, 143);
+            this.BTNA3.Name = "BTNA3";
+            this.BTNA3.Size = new System.Drawing.Size(75, 75);
+            this.BTNA3.TabIndex = 3;
+            this.BTNA3.TabStop = false;
+            this.BTNA3.UseVisualStyleBackColor = false;
+            this.BTNA3.Click += new System.EventHandler(this.BTNClicked);
+            this.BTNA3.MouseEnter += new System.EventHandler(this.BTN_Enter);
+            this.BTNA3.MouseLeave += new System.EventHandler(this.BTN_Leave);
+            // 
+            // BTNB1
+            // 
+            this.BTNB1.BackColor = System.Drawing.Color.LightGray;
+            this.BTNB1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNB1.Location = new System.Drawing.Point(135, 224);
+            this.BTNB1.Name = "BTNB1";
+            this.BTNB1.Size = new System.Drawing.Size(75, 75);
+            this.BTNB1.TabIndex = 4;
+            this.BTNB1.TabStop = false;
+            this.BTNB1.UseVisualStyleBackColor = false;
+            this.BTNB1.Click += new System.EventHandler(this.BTNClicked);
+            this.BTNB1.MouseEnter += new System.EventHandler(this.BTN_Enter);
+            this.BTNB1.MouseLeave += new System.EventHandler(this.BTN_Leave);
+            // 
+            // BTNB2
+            // 
+            this.BTNB2.BackColor = System.Drawing.Color.LightGray;
+            this.BTNB2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNB2.Location = new System.Drawing.Point(218, 224);
+            this.BTNB2.Name = "BTNB2";
+            this.BTNB2.Size = new System.Drawing.Size(75, 75);
+            this.BTNB2.TabIndex = 5;
+            this.BTNB2.TabStop = false;
+            this.BTNB2.UseVisualStyleBackColor = false;
+            this.BTNB2.Click += new System.EventHandler(this.BTNClicked);
+            this.BTNB2.MouseEnter += new System.EventHandler(this.BTN_Enter);
+            this.BTNB2.MouseLeave += new System.EventHandler(this.BTN_Leave);
+            // 
+            // BTNB3
+            // 
+            this.BTNB3.BackColor = System.Drawing.Color.LightGray;
+            this.BTNB3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNB3.Location = new System.Drawing.Point(297, 224);
+            this.BTNB3.Name = "BTNB3";
+            this.BTNB3.Size = new System.Drawing.Size(75, 75);
+            this.BTNB3.TabIndex = 6;
+            this.BTNB3.TabStop = false;
+            this.BTNB3.UseVisualStyleBackColor = false;
+            this.BTNB3.Click += new System.EventHandler(this.BTNClicked);
+            this.BTNB3.MouseEnter += new System.EventHandler(this.BTN_Enter);
+            this.BTNB3.MouseLeave += new System.EventHandler(this.BTN_Leave);
+            // 
+            // BTNC2
+            // 
+            this.BTNC2.BackColor = System.Drawing.Color.LightGray;
+            this.BTNC2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNC2.Location = new System.Drawing.Point(216, 305);
+            this.BTNC2.Name = "BTNC2";
+            this.BTNC2.Size = new System.Drawing.Size(75, 75);
+            this.BTNC2.TabIndex = 7;
+            this.BTNC2.TabStop = false;
+            this.BTNC2.UseVisualStyleBackColor = false;
+            this.BTNC2.Click += new System.EventHandler(this.BTNClicked);
+            this.BTNC2.MouseEnter += new System.EventHandler(this.BTN_Enter);
+            this.BTNC2.MouseLeave += new System.EventHandler(this.BTN_Leave);
+            // 
+            // BTNC1
+            // 
+            this.BTNC1.BackColor = System.Drawing.Color.LightGray;
+            this.BTNC1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNC1.Location = new System.Drawing.Point(135, 305);
+            this.BTNC1.Name = "BTNC1";
+            this.BTNC1.Size = new System.Drawing.Size(75, 75);
+            this.BTNC1.TabIndex = 8;
+            this.BTNC1.TabStop = false;
+            this.BTNC1.UseVisualStyleBackColor = false;
+            this.BTNC1.Click += new System.EventHandler(this.BTNClicked);
+            this.BTNC1.MouseEnter += new System.EventHandler(this.BTN_Enter);
+            this.BTNC1.MouseLeave += new System.EventHandler(this.BTN_Leave);
+            // 
+            // BTNC3
+            // 
+            this.BTNC3.BackColor = System.Drawing.Color.LightGray;
+            this.BTNC3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNC3.Location = new System.Drawing.Point(297, 305);
+            this.BTNC3.Name = "BTNC3";
+            this.BTNC3.Size = new System.Drawing.Size(75, 75);
+            this.BTNC3.TabIndex = 9;
+            this.BTNC3.TabStop = false;
+            this.BTNC3.UseVisualStyleBackColor = false;
+            this.BTNC3.Click += new System.EventHandler(this.BTNClicked);
+            this.BTNC3.MouseEnter += new System.EventHandler(this.BTN_Enter);
+            this.BTNC3.MouseLeave += new System.EventHandler(this.BTN_Leave);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(414, 444);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(75, 23);
+            this.axWindowsMediaPlayer1.TabIndex = 10;
+            this.axWindowsMediaPlayer1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(517, 511);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.BTNC3);
+            this.Controls.Add(this.BTNC1);
+            this.Controls.Add(this.BTNC2);
+            this.Controls.Add(this.BTNB3);
+            this.Controls.Add(this.BTNB2);
+            this.Controls.Add(this.BTNB1);
+            this.Controls.Add(this.BTNA3);
+            this.Controls.Add(this.BTNA2);
+            this.Controls.Add(this.BTNA1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic Tac Toe";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Button BTNA1;
+        private System.Windows.Forms.Button BTNA2;
+        private System.Windows.Forms.Button BTNA3;
+        private System.Windows.Forms.Button BTNB1;
+        private System.Windows.Forms.Button BTNB2;
+        private System.Windows.Forms.Button BTNB3;
+        private System.Windows.Forms.Button BTNC2;
+        private System.Windows.Forms.Button BTNC1;
+        private System.Windows.Forms.Button BTNC3;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.ComponentModel.BackgroundWorker WM_background;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+    }
+}
+
---
+++ TickTacToe/TickTacToe/Form1.cs
@@ -0,0 +1,367 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace TickTacToe
+{
+    public partial class Form1 : Form
+    {
+        //bool value for turns true players(X) turn false computer turn(O)
+        private bool turn = true;
+        private int turn_count = 0;
+        private bool Winner = false;
+        bool Against_Computer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            if (MessageBox.Show("Do you want to play against computer?", "Computer Player", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Against_Computer = true;
+            }
+            else
+            {
+                Against_Computer = false;
+            }
+        }
+
+        private void CheckForWinner()
+        {
+
+#region Check win conditions
+            if ((BTNA1.Text == BTNA2.Text) && (BTNA2.Text == BTNA3.Text) && (!BTNA1.Enabled))
+            {
+                Winner = true;
+            }
+            else if ((BTNB1.Text == BTNB2.Text) && (BTNB2.Text == BTNB3.Text) && (!BTNB1.Enabled))
+            {
+                Winner = true;
+            }
+            else if ((BTNC1.Text == BTNC2.Text) && (BTNC2.Text == BTNC3.Text) && (!BTNC1.Enabled))
+            {
+                Winner = true;
+            }
+            else if ((BTNA1.Text == BTNB1.Text) && (BTNB1.Text == BTNC1.Text) && (!BTNA1.Enabled))
+            {
+                Winner = true;
+            }
+            else if ((BTNA2.Text == BTNB2.Text) && (BTNB2.Text == BTNC2.Text) && (!BTNA2.Enabled))
+            {
+                Winner = true;
+            }
+            else if ((BTNA3.Text == BTNB3.Text) && (BTNB3.Text == BTNC3.Text) && (!BTNA3.Enabled))
+            {
+                Winner = true;
+            }
+            else if ((BTNA1.Text == BTNB2.Text) && (BTNB2.Text == BTNC3.Text) && (!BTNA1.Enabled))
+            {
+                Winner = true;
+            }
+            else if ((BTNA3.Text == BTNB2.Text) && (BTNB2.Text == BTNC1.Text) && (!BTNA3.Enabled))
+            {
+                Winner = true;
+            }
+            else
+            {
+                Winner = false;
+            }
+#endregion
+
+
+            if (Winner == true)
+            {
+                string WhoIsWinner = "";
+                if (turn)
+                {
+                    WhoIsWinner = "Player : O";
+                }
+                else
+                {
+                    WhoIsWinner = "Player : X";
+                }
+
+                MessageBox.Show(WhoIsWinner + "Wins!");
+                if (MessageBox.Show("Do you want to play again?", "Play Again", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    Application.Restart();
+                }
+                else
+                {
+                    Application.Exit();
+                }
+            }
+
+            if (turn_count == 8 && Winner != true )
+            {
+                MessageBox.Show("Draw!");
+                //turn_count = 0;
+            }
+        }
+
+        private void PlaySound()
+        {
+            WMPLib.WindowsMediaPlayer wmplayer = new WMPLib.WindowsMediaPlayer();
+            wmplayer.URL = "Punch.mp3";
+            wmplayer.controls.play();
+        }
+
+
+#region Tool strip menu items
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is a basic Tic Tac Toe game with a sountrack and option to play with two human players or against a Computer.Devloper: William D Scholtz  Special Thanks to Chris Merritt TickTacToe youtube video");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+
+        #endregion
+#region Button events
+        private void BTNClicked(object sender, EventArgs e)
+        {         
+            Button b = (Button)sender;
+            Winner = false;
+            //Changes the turn between x and o
+            if (turn)
+            {
+                b.Text = "X";
+                b.BackColor = Color.Aqua;
+            }
+            else
+            {
+                b.Text = "O";
+                b.BackColor = Color.Red;
+            }
+
+            //Changes the turn
+            turn = !turn;
+            //disable button so i cant be used again
+            b.Enabled = false;
+            //check if the is a winner
+            CheckForWinner();
+            PlaySound();
+            turn_count++;
+
+            //
+            if ((!turn) && (Against_Computer == true))
+            {
+                ComputerMove();
+            }
+
+
+        }
+        private void BTN_Enter(object sender, EventArgs e)
+        {
+            Button b = (Button) sender;
+            if (b.Enabled)
+            {
+                b.BackColor = Color.Cornsilk;
+                if (turn)
+                {
+                    b.Text = "X";
+                }
+                else
+                {
+                    b.Text = "O";
+                }
+            }
+        }
+
+        private void BTN_Leave(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (b.Enabled)
+            {
+                b.BackColor = Color.LightGray;
+                b.Text = "";
+            }
+        }
+        #endregion
+
+#region AI methods
+        private void ComputerMove()
+        {
+            //Rule 1 get tictac
+            //Rule 2 block x ticktax
+            //rule 3 go for coner spaces
+            //rule 4 pick open space
+
+            Button move = null;
+
+            //look for tictac
+            move = look_for_win_or_block("O");//move to win
+
+            if (move == null)
+            {
+                move = look_for_win_or_block("X");//move to block
+                if (move == null)
+                {
+                    move = look_for_coner();
+                    if (move == null)
+                    {
+                        move = look_for_open();
+                    }
+                }
+
+            }
+
+            move.PerformClick();
+        
+        }
+
+        private Button look_for_win_or_block(string mark)
+        {
+            Console.WriteLine("Looking for win or block:  " + mark);
+            //HORIZONTAL TESTS
+            if ((BTNA1.Text == mark) && (BTNA2.Text == mark) && (BTNA3.Text == ""))
+                return BTNA3;
+            if ((BTNA2.Text == mark) && (BTNA3.Text == mark) && (BTNA1.Text == ""))
+                return BTNA1;
+            if ((BTNA1.Text == mark) && (BTNA3.Text == mark) && (BTNA2.Text == ""))
+                return BTNA2;
+
+            if ((BTNB1.Text == mark) && (BTNB2.Text == mark) && (BTNB3.Text == ""))
+                return BTNB3;
+            if ((BTNB2.Text == mark) && (BTNB3.Text == mark) && (BTNB1.Text == ""))
+                return BTNB1;
+            if ((BTNB1.Text == mark) && (BTNB3.Text == mark) && (BTNB2.Text == ""))
+                return BTNB2;
+
+            if ((BTNC1.Text == mark) && (BTNC2.Text == mark) && (BTNC3.Text == ""))
+                return BTNC3;
+            if ((BTNC2.Text == mark) && (BTNC3.Text == mark) && (BTNC1.Text == ""))
+                return BTNC1;
+            if ((BTNC1.Text == mark) && (BTNC3.Text == mark) && (BTNC2.Text == ""))
+                return BTNC2;
+
+            //VERTICAL TESTS
+            if ((BTNA1.Text == mark) && (BTNB1.Text == mark) && (BTNC1.Text == ""))
+                return BTNC1;
+            if ((BTNB1.Text == mark) && (BTNC1.Text == mark) && (BTNA1.Text == ""))
+                return BTNA1;
+            if ((BTNA1.Text == mark) && (BTNC1.Text == mark) && (BTNB1.Text == ""))
+                return BTNB1;
+
+            if ((BTNA2.Text == mark) && (BTNB2.Text == mark) && (BTNC2.Text == ""))
+                return BTNC2;
+            if ((BTNB2.Text == mark) && (BTNC2.Text == mark) && (BTNA2.Text == ""))
+                return BTNA2;
+            if ((BTNA2.Text == mark) && (BTNC2.Text == mark) && (BTNB2.Text == ""))
+                return BTNB2;
+
+            if ((BTNA3.Text == mark) && (BTNB3.Text == mark) && (BTNC3.Text == ""))
+                return BTNC3;
+            if ((BTNB3.Text == mark) && (BTNC3.Text == mark) && (BTNA3.Text == ""))
+                return BTNA3;
+            if ((BTNA3.Text == mark) && (BTNC3.Text == mark) && (BTNB3.Text == ""))
+                return BTNB3;
+
+            //DIAGONAL TESTS
+            if ((BTNA1.Text == mark) && (BTNB2.Text == mark) && (BTNC3.Text == ""))
+                return BTNC3;
+            if ((BTNB2.Text == mark) && (BTNC3.Text == mark) && (BTNA1.Text == ""))
+                return BTNA1;
+            if ((BTNA1.Text == mark) && (BTNC3.Text == mark) && (BTNB2.Text == ""))
+                return BTNB2;
+
+            if ((BTNA3.Text == mark) && (BTNB2.Text == mark) && (BTNC1.Text == ""))
+                return BTNC1;
+            if ((BTNB2.Text == mark) && (BTNC1.Text == mark) && (BTNA3.Text == ""))
+                return BTNA3;
+            if ((BTNA3.Text == mark) && (BTNC1.Text == mark) && (BTNB2.Text == ""))
+                return BTNB2;
+
+            return null;
+
+        }
+
+        private Button look_for_coner()
+        {
+            Console.WriteLine("Looking for corner");
+            if (BTNA1.Text == "O")
+            {
+                if (BTNA3.Text == "")
+                    return BTNA3;
+                if (BTNC3.Text == "")
+                    return BTNC3;
+                if (BTNC1.Text == "")
+                    return BTNC1;
+            }
+
+            if (BTNA3.Text == "O")
+            {
+                if (BTNA1.Text == "")
+                    return BTNA1;
+                if (BTNC3.Text == "")
+                    return BTNC3;
+                if (BTNC1.Text == "")
+                    return BTNC1;
+            }
+
+            if (BTNC3.Text == "O")
+            {
+                if (BTNA1.Text == "")
+                    return BTNA3;
+                if (BTNA3.Text == "")
+                    return BTNA3;
+                if (BTNC1.Text == "")
+                    return BTNC1;
+            }
+
+            if (BTNC1.Text == "O")
+            {
+                if (BTNA1.Text == "")
+                    return BTNA3;
+                if (BTNA3.Text == "")
+                    return BTNA3;
+                if (BTNC3.Text == "")
+                    return BTNC3;
+            }
+
+            if (BTNA1.Text == "")
+                return BTNA1;
+            if (BTNA3.Text == "")
+                return BTNA3;
+            if (BTNC1.Text == "")
+                return BTNC1;
+            if (BTNC3.Text == "")
+                return BTNC3;
+
+            return null;
+        }
+        private Button look_for_open()
+        {
+            Console.WriteLine("Looking for open space");
+            Button b = null;
+            foreach (Control c in Controls)
+            {
+                b = c as Button;
+                if (b != null)
+                {
+                    if (b.Text == "")
+                        return b;
+                }
+            }
+
+            return null;
+        }
+
+#endregion
+    }
+}
